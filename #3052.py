1717f341b371990b685f7fa2839ee486b692d657
portals2 <dkfvkswotjd@naver.com>
2022-12-29 11:40:37 +0900

    221229

--- #3052.py
+++ #3052.py
@@ -1,8 +1,8 @@
-# rest = [i%42 for i in [int(input()) for j in range(10)]]
-# rest = set(rest)
-# rest = list(rest)
-# print(len(rest))
+rest = [i%42 for i in [int(input()) for j in range(10)]]
+rest = set(rest)
+rest = list(rest)
+print(len(rest))
 
-a=set()
-for i in map(int,open(0).read().split()):a.add(i%42)
-print(len(a))
+# a=set()
+# for i in map(int,open(0).read().split()):a.add(i%42)
+# print(len(a))
