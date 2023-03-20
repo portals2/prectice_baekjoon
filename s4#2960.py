f1f05c104b749fca91549700030477a3a6763536
portals2 <dkfvkswotjd@naver.com>
2023-03-20 11:59:27 +0900

    230320

---
+++ s4#2960.py
@@ -0,0 +1,24 @@
+n, k = map(int, input().split(' '))
+con=0
+
+
+a = [False] + [True]*(n)
+l = []
+
+for i in range(2,n+1):
+  if a[i]:
+    for j in range(i, n+1, i):
+        if j in l:
+           pass
+        else:
+            a[j] = False
+            l.append(j)
+            if len(l) == k:
+               print(j)
+
+# for i in p:
+#     if i in primes:
+#         con +=1
+    
+# print(con)
+
