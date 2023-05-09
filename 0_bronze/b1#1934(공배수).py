ff59a9f6191be1ebedf4b8558e3b9b093a418778
portals2 <dkfvkswotjd@naver.com>
2023-05-09 21:16:52 +0900

    230509_a

---
+++ 0_bronze/b1#1934(공배수).py
@@ -0,0 +1,32 @@
+# import sys
+
+# t = int(sys.stdin.readline())
+
+# for _ in range(t):
+#     a, b = map(int, sys.stdin.readline().split())
+#     answer = 1
+#     num = 2
+#     while (num <= a and num <= b):
+#         if a % num == 0 and b % num == 0:
+#             answer *= num
+#             a //= num
+#             b //= num
+#         else:
+#             num += 1
+        
+#     answer *= (a*b)
+#     print(answer)
+
+
+
+####
+import sys
+T=int(sys.stdin.readline())
+for _ in range(T):
+    x,y=map(int,sys.stdin.readline().split())
+    k = 0
+    for i in range(1,min(x,y)+1):
+        if x%i==0 and y%i==0:
+            k = i
+    print(x*y//k)
+###
