4631c2f50ffda33b312da3392fc4e704e2b4208d
portals2 <dkfvkswotjd@naver.com>
2023-05-16 11:46:28 +0900

    230516

--- 0_bronze/b4#2083.py
+++ 0_bronze/b4#2083.py
@@ -3,7 +3,7 @@ while True:
     if a =='#':
         break
 
-    if int(b) >= 17 or int(c) >= 80:
+    if int(b) > 17 or int(c) >= 80:
         print(a,'Senior')
     else:
         print(a,'Junior')
