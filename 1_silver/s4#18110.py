009a2af1fa8dd83950ff51da56acfecb184d3297
portals2 <dkfvkswotjd@naver.com>
2023-06-17 10:55:18 +0900

    230617

--- 1_silver/s4#18110.py
+++ 1_silver/s4#18110.py
@@ -1,15 +1,17 @@
 import math
 n = int(input())
 
-a = []
+a = [0 for _ in range(n)]
 
 for i in range(n):
-    a.append(int(input()))
+    a[i] = (int(input()))
 
 a.sort()
 avg = math.floor(n*(15/100)+0.5)
 
-if n < 4:
+if n == 0:
+    print(0)
+elif n < 4:
     print(sum(a)//n)
 else:
     print(math.floor((sum(a[avg:-avg])/len(a[avg:-avg])+0.5)))
