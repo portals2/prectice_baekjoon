37746ac83e676beee72fd02767a5ca9d539cd531
portals2 <dkfvkswotjd@naver.com>
2023-06-23 11:19:07 +0900

    230623

---
+++ 1_silver/s3#15651(backtracking).py
@@ -0,0 +1,18 @@
+import sys
+
+n,m = map(int,sys.stdin.readline().split())
+result = []
+
+def backTracking(start):
+  if len(result) == m:
+    for i in range(m):
+      print(result[i], end=" ")
+    print("")
+    return
+
+  for i in range(1,n+1): # 1부터 시작해야함
+      result.append(i) # 중복 숫자 제거 if문 삭제
+      backTracking(i)
+      result.pop()
+      
+backTracking(1)
